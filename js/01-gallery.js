c582f6b5c677ffef95db3b1164ddcebbfc6c54df
Savitsky95 <117753225+Savitsky95@users.noreply.github.com>
2023-05-09 14:53:47 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,7 @@ const galleryList = document.querySelector(".gallery");
 // Create and render gallery items
 const createGalleryItem = ({ preview, original, description }) => `
   <li class="gallery__item">
-    <a class="gallery__link" href="${original}" data-original-img=${original}>
+    <a class="gallery__link" href="${original}" data-original-img="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
   </li>
@@ -13,31 +13,27 @@ const createGalleryItem = ({ preview, original, description }) => `
 
 galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
 
-// Store instances in an array
-const instances = [];
-
 // Show corresponding instance when image is clicked
 galleryList.addEventListener("click", (event) => {
   event.preventDefault();
-  const original = event.target
-    .closest(".gallery__link")
-    .getAttribute("data-original-img");
-  const instance = basicLightbox.create(`
-    <img src="${original}" width="800" height="600">
-  `);
-  instances.push(instance); // Add instance to array
-  instance.show();
-  document.addEventListener("keydown", (event) => onEscPress(event, instance));
+  if (event.target.classList.contains("gallery__image")) {
+    const original = event.target
+      .closest(".gallery__link")
+      .getAttribute("data-original-img");
+    const instance = basicLightbox.create(`
+      <img src="${original}" width="800" height="600">
+    `);
+    instance.show();
+    document.addEventListener("keydown", onEscPress);
+  }
 });
 
 // Close instance on Esc key press
-const onEscPress = (event, instance) => {
+const onEscPress = (event) => {
   const ESC_KEYCODE = "Escape";
   if (event.code === ESC_KEYCODE) {
+    const instance = document.querySelector(".basicLightbox");
     instance.close();
-    instances.splice(instances.indexOf(instance), 1); // Remove instance from array
-    document.removeEventListener("keydown", (event) =>
-      onEscPress(event, instance)
-    );
+    document.removeEventListener("keydown", onEscPress);
   }
 };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryList = document.querySelector(".gallery");
 
@@ -8,22 +7,17 @@ galleryItems.forEach(({ preview, original, description }) => {
   const galleryItem = document.createElement("li");
   galleryItem.classList.add("gallery__item");
 
-  const link = document.createElement("a");
-  link.classList.add("gallery__link");
-  link.href = original;
+  galleryItem.innerHTML = `
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  `;
 
-  const image = document.createElement("img");
-  image.classList.add("gallery__image");
-  image.src = preview;
-  image.alt = description;
-
-  link.append(image);
-  galleryItem.append(link);
   galleryList.append(galleryItem);
 });
 
 // Initialize SimpleLightbox
-const lightbox = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
